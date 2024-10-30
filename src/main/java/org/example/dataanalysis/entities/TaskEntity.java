639643e6a7318224e9eb198122cd5c1eaa0a90e4
Forvi <nforvi@mail.ru>
2024-10-30 18:33:58 +0500

    task1

---
+++ src/main/java/org/example/dataanalysis/entities/TaskEntity.java
@@ -0,0 +1,31 @@
+﻿package org.example.dataanalysis.entities;
+
+import java.util.UUID;
+
+public class TaskEntity {
+    private UUID id;
+    private String title;
+    private String description;
+
+    public TaskEntity(String Title, String Description) {
+        this.id = UUID.randomUUID();
+        this.title = Title;
+        this.description = Description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String toString() {
+        return String.format("Id: %s, Title: %s, Description: %s", title, description);
+    }
+}
